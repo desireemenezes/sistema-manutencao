a59b69b738f22a4e61b3381c070c01d5f3497fde
desireemenezes <deemenezes@hotmail.com>
2025-06-25 19:20:55 -0300

    chore: add rules to ignore environment variable files (.env)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://rest-json-server.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
